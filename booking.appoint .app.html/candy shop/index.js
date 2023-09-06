f711bccb51aa4a364791a3301d234bc222b9ebb1
aparna00006 <as7236461@gmail.com>
2023-09-05 23:05:38 -0700

    candy shop with async await

--- booking.appoint .app.html/candy shop/index.js
+++ booking.appoint .app.html/candy shop/index.js
@@ -1,58 +1,59 @@
-function saveToLocalStorage(event) {
-    event.preventDefault();
-    const candy = event.target.CandyName.value;
-    const description = event.target.description.value;
-    const price = event.target.Price.value;
-    let quantity = event.target.Quantity.value;
-  
-    let obj = {
-      candy,
-      description,
-      price,
-      quantity,
-    };
-    axios.post("https://crudcrud.com/api/c80ffa353cc24a40a5c0884eb8274354/CandyShop",obj)
-    .then((res)=>{
-        showUserOnScreen(res.data);
-       })
-       .catch((err)=>{
-          console.log(err);
-       });
+async function saveToLocalStorage(event) {
+  event.preventDefault();
+  const candy = event.target.CandyName.value;
+  const description = event.target.description.value;
+  const price = event.target.Price.value;
+  let quantity = event.target.Quantity.value;
+
+  let obj = {
+    candy,
+    description,
+    price,
+    quantity,
+  };
+  try {
+    const postData = await axios.post("https://crudcrud.com/api/6c01f7c4d8634e4fba9a5e9bf864e584/CandyShop",obj);
+    showUserOnScreen(postData.data);
+  } catch (err) {
+    alert(err.message);
+  }
 }
-   window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/c80ffa353cc24a40a5c0884eb8274354/CandyShop")
-    .then((res)=>{
-      for (let i = 0; i < res.data.length; i++) {
-        showUserOnScreen(res.data[i]);
-      };
-    }) 
-    .catch ((err)=>{
-      showUserOnScreen(err);
-    })
-  });
-  
-   function showUserOnScreen(obj) {
-    const parentEl = document.querySelector("#formElements");
-    const childEl = document.createElement("li");
-    childEl.textContent = `${obj.candy} - ${obj.description} - ${obj.price} - ${obj.quantity}`
-    childEl.classList = "candy-details";
-    parentEl.appendChild(childEl);
-    const btn1 = document.createElement("input");
-    btn1.type = "button";
-    btn1.value = "Buy1";
-    childEl.appendChild(btn1);
-    const btn2 = document.createElement("input");
-    btn2.type = "button";
-    btn2.value = "Buy2";
-    childEl.appendChild(btn2);
-    const btn3 = document.createElement("input");
-    btn3.type = "button";
-    btn3.value = "Buy3";
-    childEl.appendChild(btn3);
-  
-    // button click
-    btn1.onclick =  () => {
-        axios.put(`https://crudcrud.com/api/c80ffa353cc24a40a5c0884eb8274354/CandyShop/${obj._id}`,
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const windowData = await axios.get("https://crudcrud.com/api/6c01f7c4d8634e4fba9a5e9bf864e584/CandyShop");
+    for (let i = 0; i < windowData.data.length; i++) {
+      showUserOnScreen(windowData.data[i]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+function showUserOnScreen(obj) {
+  const parentEl = document.querySelector("#formElements");
+  const childEl = document.createElement("li");
+  childEl.textContent = `${obj.candy} - ${obj.description} - ${obj.price} - ${obj.quantity}`;
+  childEl.classList = "candy-details";
+  parentEl.appendChild(childEl);
+  const btn1 = document.createElement("input");
+  btn1.type = "button";
+  btn1.value = "Buy1";
+  childEl.appendChild(btn1);
+  const btn2 = document.createElement("input");
+  btn2.type = "button";
+  btn2.value = "Buy2";
+  childEl.appendChild(btn2);
+  const btn3 = document.createElement("input");
+  btn3.type = "button";
+  btn3.value = "Buy3";
+  childEl.appendChild(btn3);
+
+  // button click
+  btn1.onclick = async () => {
+    if (obj.quantity >= 1) {
+      try {
+        const dataChange = await axios.put(
+          `https://crudcrud.com/api/6c01f7c4d8634e4fba9a5e9bf864e584/CandyShop/${obj._id}`,
           {
             candy: `${obj.candy}`,
             description: `${obj.description}`,
@@ -60,15 +61,23 @@ function saveToLocalStorage(event) {
             quantity: `${obj.quantity - 1}`,
           }
         );
-  
-        axios.get(`https://crudcrud.com/api/c80ffa353cc24a40a5c0884eb8274354/CandyShop/${obj._id}`)
-        .then((res)=>{
-        showUserOnScreen(res.data);
+        const getData = await axios.get(
+          `https://crudcrud.com/api/6c01f7c4d8634e4fba9a5e9bf864e584/CandyShop/${obj._id}`
+        );
+        showUserOnScreen(getData.data);
         parentEl.removeChild(childEl);
-      }) .catch ((err)=> console.log(err))
+      } catch (err) {
+        console.log(err);
       }
-    btn2.onclick =  () => {
-    axios.put(`https://crudcrud.com/api/c80ffa353cc24a40a5c0884eb8274354/CandyShop/${obj._id}`,
+    } else {
+      alert("quantity is not available");
+    }
+  };
+  btn2.onclick = async () => {
+    if (obj.quantity >= 2) {
+      try {
+        const dataChange2 = await axios.put(
+          `https://crudcrud.com/api/6c01f7c4d8634e4fba9a5e9bf864e584/CandyShop/${obj._id}`,
           {
             candy: `${obj.candy}`,
             description: `${obj.description}`,
@@ -76,15 +85,23 @@ function saveToLocalStorage(event) {
             quantity: `${obj.quantity - 2}`,
           }
         );
-  
-        axios.get(`https://crudcrud.com/api/c80ffa353cc24a40a5c0884eb8274354/CandyShop/${obj._id}`)
-        .then((res)=>{
-            showUserOnScreen(res.data);
-            parentEl.removeChild(childEl);
-          }) .catch ((err)=> console.log(err))
-          }
-    btn3.onclick = () => {
-      axios.put(`https://crudcrud.com/api/c80ffa353cc24a40a5c0884eb8274354/CandyShop/${obj._id}`,
+        const getData2 = await axios.get(
+          `https://crudcrud.com/api/6c01f7c4d8634e4fba9a5e9bf864e584/CandyShop/${obj._id}`
+        );
+        showUserOnScreen(getData2.data);
+        parentEl.removeChild(childEl);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      alert("quantity is not available");
+    }
+  };
+  btn3.onclick = async () => {
+    if (obj.quantity >= 3) {
+      try {
+        const dataChange3 = await axios.put(
+          `https://crudcrud.com/api/6c01f7c4d8634e4fba9a5e9bf864e584/CandyShop/${obj._id}`,
           {
             candy: `${obj.candy}`,
             description: `${obj.description}`,
@@ -92,12 +109,16 @@ function saveToLocalStorage(event) {
             quantity: `${obj.quantity - 3}`,
           }
         );
-        axios.get(`https://crudcrud.com/api/c80ffa353cc24a40a5c0884eb8274354/CandyShop/${obj._id}`)
-        .then((res)=>{
-            showUserOnScreen(res.data);
-            parentEl.removeChild(childEl);
-          }) .catch ((err)=> console.log(err))
-            
-          }
-        
-   } ; 
+        const getData3 = await axios.get(
+          `https://crudcrud.com/api/6c01f7c4d8634e4fba9a5e9bf864e584/CandyShop/${obj._id}`
+        );
+        showUserOnScreen(getData3.data);
+        parentEl.removeChild(childEl);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      alert("quantity is not available");
+    }
+  };
+}
