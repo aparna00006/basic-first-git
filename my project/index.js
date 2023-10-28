dbdc4106d5857be237356efd8d27e49cc80ecf03
aparna00006 <as7236461@gmail.com>
2023-10-28 01:28:48 -0700

    insta love Btn

--- my project/index.js
+++ my project/index.js
@@ -1,17 +1,16 @@
-var istatus= document.querySelector("h5")
-var btn = document.querySelector("#add")
-var check =0
+var con = document.querySelector('#container')
+var love = document.querySelector("i")
 
-btn.addEventListener("click", function(){
-    if(check==0){
-        istatus.innerHTML="Friends"
-        istatus.style.color = "green"  
-        btn.innerHTML = "Remove Friend"
-        check=1
-    }else{
-        istatus.innerHTML="Stranger"
-        istatus.style.color = "red"  
-        btn.innerHTML = "Add Friend"
-        check=0
-    }
-})
+
+con.addEventListener("dbclick", function(){
+    love.style.transform = 'translate(-50%,-50%) scale(1)'
+    love.style.opacity = 0.8
+    setTimeout(function(){
+        love.style.opacity = 0;
+    },1000);
+    setTimeout(function(){
+        love.style.transform = "translate(-50%,-50%) scale(0)";
+    },2000);
+});
+
+// insta love btn
